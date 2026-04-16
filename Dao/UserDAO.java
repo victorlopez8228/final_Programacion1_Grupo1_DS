19ae638a0d988040d48ec940b4ec502b53a9f5f1
rramses390 <rramses390@gmail.com>
2026-04-15 21:34:02 -0500

    importacion de la base de datos y las carpetas del git, con la validacion de usuario

--- Dao/UserDAO.java
+++ Dao/UserDAO.java
@@ -1,12 +1,15 @@
 package Dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import db.ConnectionDB;
 import model.User;
 
 public class UserDAO {
 
+  // acceso de datos para usuarios
   public boolean insertNewUser(User user) {
     String sql = "INSERT INTO users (username, password, email, is_active) VALUES (?, ?, ?, ?)";
     try (PreparedStatement preparedStatement = ConnectionDB.getConnection().prepareStatement(sql)) {
@@ -21,4 +24,23 @@ public class UserDAO {
       return false;
     }
   }
+
 }
+
+  // verifica si ya existe un usuario con ese username en la BD.
+public boolean existUsername(String username) {
+        String sql = "SELECT COUNT(*) FROM users WHERE username = ?";
+        try (PreparedStatement preparedStatement = ConnectionDB.getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, username);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                return count > 0;
+            }
+        
+            return false;
+        } catch (SQLException e) {
+            System.err.println("Error verificar username: " + e.getMessage());
+            return false;
+        }
+    }
